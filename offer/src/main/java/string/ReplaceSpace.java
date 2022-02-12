e82f2549ac23844aa6a75f23582c3dc3c4fe5440
lineng <945688026@qq.com>
2022-02-12 23:22:33 +0800

    [update]replaceSpace

---
+++ offer/src/main/java/string/ReplaceSpace.java
@@ -0,0 +1,62 @@
+package string;
+
+/**
+ * Easy
+ */
+public class ReplaceSpace {
+
+    public String replaceSpace1(String str) {
+        if (str == null) {
+            return null;
+        }
+        //选用 StringBuilder 单线程使用，比较快，选不选都行
+        StringBuilder sb = new StringBuilder();
+        //使用 sb 逐个复制 str ，碰到空格则替换，否则直接复制
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public String replaceSpace(String s) {
+
+        if(s == null || s.length() == 0){
+            return s;
+        }
+        //扩充空间，空格数量2倍
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' '){
+                str.append("  ");
+            }
+        }
+        //若是没有空格直接返回
+        if(str.length() == 0){
+            return s;
+        }
+        //有空格情况 定义两个指针
+        int left = s.length() - 1;//左指针：指向原始字符串最后一个位置
+        s += str.toString();
+        int right = s.length()-1;//右指针：指向扩展字符串的最后一个位置
+        char[] chars = s.toCharArray();
+        while(left>=0){
+            if(chars[left] == ' '){
+                chars[right--] = '0';
+                chars[right--] = '2';
+                chars[right] = '%';
+            }else{
+                chars[right] = chars[left];
+            }
+            left--;
+            right--;
+        }
+        return new String(chars);
+
+    }
+
+}

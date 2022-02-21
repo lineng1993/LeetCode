1b8f9826b7ec8b516d8bee4f8cddfd14d64c8674
lineng <945688026@qq.com>
2022-02-21 23:13:23 +0800

    [update]array:33

--- leetcode/array/searchInRotatedSortedArray33/SearchInRotatedSortedArray.java
+++ leetcode/array/searchInRotatedSortedArray33/SearchInRotatedSortedArray.java
@@ -10,8 +10,8 @@ public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
 
         int[] nums = {6, 7, 0, 1, 2, 4, 5};
-        int[] nums2 = { 4, 5, 6, 7, 0,  1, 2};
-//        System.out.println(new SearchInRotatedSortedArray().search2(nums, 4));
+        int[] nums2 = { 4, 5, 6, 7, 0,  1};
+        System.out.println(new SearchInRotatedSortedArray().search2(nums, 4));
         System.out.println(new SearchInRotatedSortedArray().search2(nums2, 5));
     }
 
@@ -36,30 +36,32 @@ public class SearchInRotatedSortedArray {
          */
 
         int n = nums.length;
-        //base
-        if (n == 0) return -1;
-        if (n == 1) return nums[0] == target ? 0 : -1;
-
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
         int l = 0, r = n - 1;
-        while (l <= r){
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) return mid;
-
-            if (nums[0] < nums[mid]){
-                if (nums[0] <= target && target < nums[mid]){
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
                     r = mid - 1;
-                }else {
+                } else {
                     l = mid + 1;
                 }
-            }else {
-                if (nums[mid] < target && target <= nums[n-1]){
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
                     l = mid + 1;
-                }else {
+                } else {
                     r = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 

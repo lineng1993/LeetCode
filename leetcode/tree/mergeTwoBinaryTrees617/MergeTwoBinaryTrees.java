73a06eb450f853d590af40a2af223ecb89a59cc3
lineng <945688026@qq.com>
2022-04-29 18:38:33 +0800

    [update]617

--- leetcode/tree/mergeTwoBinaryTrees617/MergeTwoBinaryTrees.java
+++ leetcode/tree/mergeTwoBinaryTrees617/MergeTwoBinaryTrees.java
@@ -12,7 +12,7 @@ public class MergeTwoBinaryTrees {
 
         TreeNode root1 = TreeNode.int2TreeNode(1,2,3,4,5);
         TreeNode root2 = TreeNode.int2TreeNode(1,2,3,4,5,6,7);
-        System.out.println(new MergeTwoBinaryTrees().mergeTrees(root1, root2));
+        System.out.println(new MergeTwoBinaryTrees().mergeTrees2(root1, root2));
 
     }
 
@@ -30,6 +30,33 @@ public class MergeTwoBinaryTrees {
         root.left = mergeTrees(root1.left, root2.left);
         root.right = mergeTrees(root1.right, root2.right);
         return root;
+    }
+
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null && root2 == null) return null;
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.val += root2.val;
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+
+    public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null && root2 == null) return null;
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        root1.val += root2.val;
+        return root1;
     }
 
 }
